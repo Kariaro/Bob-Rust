352ba7dfc2c9facfd9945e155fd77e309df5351e
Kariaro <25615845+Kariaro@users.noreply.github.com>
2021-03-17 20:05:44 +0100

    Improved the rendering window

--- src/main/sorter/com/bobrust/sorter/BorstSortingExampleWindow.java
+++ src/main/sorter/com/bobrust/sorter/BorstSortingExampleWindow.java
@@ -3,7 +3,6 @@ package com.bobrust.sorter;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferStrategy;
 import java.io.File;
 import java.util.List;
 
@@ -25,65 +24,36 @@ public class BorstSortingExampleWindow extends JFrame {
 		new BorstSortingExampleWindow();
 	}
 	
-	private BufferStrategy bs;
-	private boolean do_render = true;
-	private Thread render_thread = new Thread(new Runnable() {
-		boolean hasThread = false;
-		
-		public void run() {
-			while(true) {
-				try {
-					Thread.sleep(10);
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-				
-				if(bs != null) {
-					bs.show();
-				}
-				
-				if(do_render && !hasThread) {
-					hasThread = true;
-					Thread thread = new Thread(() -> {
-						render();
-						hasThread = false;
-					});
-					thread.setDaemon(true);
-					thread.start();
-					do_render = false;
-				}
-			}
-		}
-	});
-	
 	public BorstSortingExampleWindow() {
 		setSize(512 * 2 + 1, 512 + 29);
 		setResizable(false);
-		setTitle("Example painting");
+		setTitle("Example sorting");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 		addKeyListener(new KeyAdapter() {
-			@Override
 			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_U) {
+				repaint();
+			}
+			
+			public void keyTyped(KeyEvent e) {
+				if(Character.toLowerCase(e.getKeyChar()) == 'u') {
 					refreshBorst();
 				}
 				
-				do_render = true;
+				repaint();
 			}
 		});
 		setIgnoreRepaint(true);
 		
 		refreshBorst();
 		repaint();
-		
-		render_thread.setDaemon(true);
-		render_thread.start();
 	}
+	
+	private int shapes = 6000;
 	
 	private void refreshBorst() {
 		try {
-			BorstData borst = BorstReader.readFile(new File("res/borst/monalisarust8000.borst"));
+			BorstData borst = BorstReader.readFile(new File("res/borst/wolf8000.borst"));
 			
 			list = new BlobList();
 			
@@ -97,11 +67,41 @@ public class BorstSortingExampleWindow extends JFrame {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			long start0 = System.nanoTime();
+			BlobList sorted0 = FastBlobSorterV2.sort(list);
+			long time0 = System.nanoTime() - start0;
+			
+			long start1 = System.nanoTime();
+			BlobList sorted1 = FastBlobSorter.sort(list);
+			long time1 = System.nanoTime() - start1;
+			
+			sorted = sorted1;
+			
+			{
+				int a0 = score(list, shapes);
+				int a1 = score(sorted1, shapes);
+				int a2 = score(sorted0, shapes);
+				int ms = 50;
+				
+				System.out.println("=============================================================================");
+				System.out.printf("Timings: Basic: %.2f ms, Fast: %.2f ms\n", time0 / 1000000.0, time1 / 1000000.0);
+				System.out.printf("Shapes: %s\n", shapes);
+				System.out.printf("Original: score=%s,\t~(%s)\n", a0, timeStr((a0 + shapes) * ms));
+				System.out.printf("Sorted  : score=%s,\t~(%s)\n", a1, timeStr((a1 + shapes) * ms));
+				System.out.printf("Old     : score=%s,\t~(%s)\n", a2, timeStr((a2 + shapes) * ms));
+				System.out.printf("Saved   : ~(%s)\n", timeStr(Math.abs(a1 - a0) * ms));
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private BlobList list;
-	private BlobPainter render = new BlobPainter();
 	private static final Color BACKGROUND = new Color(0xebeace);
+	private BlobPainter render = new BlobPainter();
+	private BlobList sorted;
+	private BlobList list;
 	
 	public String timeStr(int millis) {
 		int minutes = (int)(millis / 60000);
@@ -115,26 +115,8 @@ public class BorstSortingExampleWindow extends JFrame {
 	}
 	
 	@Override
-	public void paint(Graphics g) {
-		if(bs == null) {
-			createBufferStrategy(1);
-			bs = getBufferStrategy();
-			return;
-		}
-	}
-	
-	@Override
-	public void paintComponents(Graphics g) {
-		
-	}
-	
-	@Override
-	public void paintAll(Graphics g) {
-		
-	}
-	
-	protected void render() {
-		Graphics2D g = (Graphics2D)bs.getDrawGraphics();
+	public void paint(Graphics gr) {
+		Graphics2D g = (Graphics2D)gr;
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		//g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
 		//g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -144,34 +126,13 @@ public class BorstSortingExampleWindow extends JFrame {
 		BlobList data = list;
 		if(data == null) return;
 		
-		int shapes = 6000;
-		render.draw(g, shapes, BACKGROUND, data);
+		shapes = 8000;
+		BlobList list0 = list;
+		if(list0 != null) render.draw(g, shapes, BACKGROUND, data);
 		g.translate(512, 0);
 		
-		long start0 = System.nanoTime();
-		BlobList sorted0 = FastBlobSorterV2.sort(data);
-		long time0 = System.nanoTime() - start0;
-		
-		long start1 = System.nanoTime();
-		BlobList sorted1 = FastBlobSorter.sort(data);
-		long time1 = System.nanoTime() - start1;
-		
-		BlobList sorted = sorted1;
-		render.draw(g, shapes, BACKGROUND, sorted);
-		
-		{
-			int a0 = score(data, shapes);
-			int a1 = score(sorted, shapes);
-			int a2 = score(sorted0, shapes);
-			int ms = 50;
-			
-			System.out.printf("Timings: Basic: %.2f ms, Fast: %.2f ms\n", time0 / 1000000.0, time1 / 1000000.0);
-			System.out.printf("Shapes: %s\n", shapes);
-			System.out.printf("Original: score=%s,\t~(%s)\n", a0, timeStr((a0 + shapes) * ms));
-			System.out.printf("Sorted  : score=%s,\t~(%s)\n", a1, timeStr((a1 + shapes) * ms));
-			System.out.printf("Old     : score=%s,\t~(%s)\n", a2, timeStr((a2 + shapes) * ms));
-			System.out.printf("Saved   : ~(%s)\n", timeStr(Math.abs(a1 - a0) * ms));
-		}
+		BlobList list1 = sorted;
+		if(list1 != null) render.draw(g, shapes, BACKGROUND, list1);
 	}
 	
 	public int score(BlobList data) {
@@ -198,9 +159,5 @@ public class BorstSortingExampleWindow extends JFrame {
 		}
 		
 		return changes;
-	}
-	
-	public void repaint() {
-		
 	}
 }
